273d216ffb334b212553edee59cd9058600bbafe
yash viradiyah <yashviradiyah7@gmail.com>
2018-05-09 10:16:15 +0530

    Splash Page added,set bg color to white. Named App: Bill spliter & tax counter

--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.dell.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, FirstPage.class);
+                startActivity(homeIntent);
+                finish();
+
+
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
