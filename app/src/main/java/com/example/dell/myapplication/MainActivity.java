e35d8be4a44eb5bf6ca31877dfef3aa73aa2855b
Jasu <lathiyajasmit@gmail.com>
2018-06-10 21:08:29 +0530

    App icon changed

--- app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
